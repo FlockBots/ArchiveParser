e7d91e44b317696d128fe681d47fc0c67f1ee8dd
Stan Janssen <stanjanssen@outlook.com>
2017-10-30 09:04:19 +0100

    download archive script

---
+++ download.sh
@@ -0,0 +1,2 @@
+NOW="$(date +'%Y%m%d')"
+wget -O "archive.$NOW.csv" https://docs.google.com/spreadsheets/d/1X1HTxkI6SqsdpNSkSSivMzpxNT-oeTbjFFDdEkXD30o/export?format=csv&id=1X1HTxkI6SqsdpNSkSSivMzpxNT-oeTbjFFDdEkXD30o
