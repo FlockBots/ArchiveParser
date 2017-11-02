5829105a936910b733cb1106f7152688c877f0fc
Stan Janssen <stanjanssen@outlook.com>
2017-11-02 16:30:06 +0100

    cron job

---
+++ auto_parse.sh
@@ -0,0 +1,4 @@
+cd $(dirname "$0")
+wget -O "/tmp/archive.csv" https://docs.google.com/spreadsheets/d/1X1HTxkI6SqsdpNSkSSivMzpxNT-oeTbjFFDdEkXD30o/export?format=csv&id=1X1HTxkI6SqsdpNSkSSivMzpxNT-oeTbjFFDdEkXD30o
+
+bundle exec ruby cli.rb --source /tmp/archive.csv --target archive.db
