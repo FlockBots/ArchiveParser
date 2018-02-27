b7a431cfa73b49a8a468e83ecc8ee525763be4f0
Stan Janssen <mail@janssen.io>
2018-02-27 19:52:54 +0000

    properly download csv

--- auto_parse.sh
+++ auto_parse.sh
@@ -1,4 +1,4 @@
 cd $(dirname "$0")
-wget -O "/tmp/archive.csv" https://docs.google.com/spreadsheets/d/1X1HTxkI6SqsdpNSkSSivMzpxNT-oeTbjFFDdEkXD30o/export?format=csv&id=1X1HTxkI6SqsdpNSkSSivMzpxNT-oeTbjFFDdEkXD30o
-
+wget -O "/tmp/archive.csv" "https://docs.google.com/spreadsheets/d/1X1HTxkI6SqsdpNSkSSivMzpxNT-oeTbjFFDdEkXD30o/export?format=csv&id=1X1HTxkI6SqsdpNSkSSivMzpxNT-oeTbjFFDdEkXD30o"
+echo 'done downloading'
 bundle exec ruby cli.rb --source /tmp/archive.csv --target archive.db
